2fcf83b188adb0132e2bac77570a813a3d294c5c
Ashish Karki <akarki@genscape.com>
2017-10-20 12:25:45 -0400

    made it to part 5 of the tutorial. there is all navigation in place.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,23 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 import { HeroDetailComponent } from "./hero-detail.component";
 import { HeroesComponent } from "./heroes.component";
 import { HeroService } from "./hero.service";
 import { DashboardComponent } from "./dashboard.component";
 
+import { AppRoutingModule } from "./app-routing.module";
+
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
+  imports: [
+    BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      }
-    ]) 
+    AppRoutingModule
   ],
-  declarations: [ 
-    AppComponent ,
+  declarations: [
+    AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     DashboardComponent
@@ -38,6 +25,6 @@ import { DashboardComponent } from "./dashboard.component";
   providers: [
     HeroService
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
